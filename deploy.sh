b557208c8aeee9435402adaa2ee8d15c0edbe70b
moaalaa <moaalaa16@gmail.com>
2020-01-29 15:34:33 +0200

    Added: Adding Deploying Script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+#File From Vuepress Official Site With Some Edits
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com:moaalaa/VuePress-Simple-Docs.git master:gh-pages
+
+cd -
